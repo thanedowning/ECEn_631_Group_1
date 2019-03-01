91867dca061a0935e04862028631458eff93e0fc
Seth Nielsen <sethmnielsen@gmail.com>
2019-02-28 18:38:38 -0700

    Got blob detection kinda working in python

--- plinko/tennisDetector_vid.py
+++ plinko/tennisDetector_vid.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # This script attempts HSV tennis ball detection on a video file or
 # camera feed.
@@ -11,21 +11,40 @@ import numpy as np
 import os
 
 ######## Specify video file or camera (i.e. '/dev/video1') here ########
-cap = cv2.VideoCapture('/home/seth/Videos/A4.MOV')
+# cap = cv2.VideoCapture('/home/seth/Videos/A4.MOV')
+cap = cv2.VideoCapture(0)
 
 alpha = 0.05
 prev_score = 0
 while cap.isOpened():
     # Read image
     ret, im = cap.read()
-    hsv = cv2.cvtColor(im, cv2.COLOR_RGB2HSV);
+    hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV);
+    # green = im[:,:,1]
+
+    hue = hsv[:,:,0]
+    sat = hsv[:,:,1]
+    val = hsv[:,:,2]
+
+    # mask = cv2.inRange(hue, 75, 100)
+    # mask = cv2.inRange(sat, 200, 255)
+    # mask = cv2.inRange(val, 100, 255)
+
+    # cv2.imshow('filter', mask)
+    # cv2.waitKey(10)
+    # continue
 
     # define range of blue color in HSV
-    lower_blue = np.array([69,60,90])
-    upper_blue  = np.array([91,255,255])
+    lower_blue = np.array([75,200,100])
+    upper_blue  = np.array([100,255,255])
+    # lower = np.array([0,150,0])
+    # upper = np.array([255,255,255])
+    # lower = np.array([100])
+    # upper = np.array([255])
 
     # Threshold the HSV image to get only blue colors
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    # mask = cv2.inRange(green, lower, upper)
 
     kernel = np.ones((8,8),np.uint8)
     erosion = cv2.erode(mask,kernel,iterations = 1)
@@ -40,22 +59,22 @@ while cap.isOpened():
 
     # Change thresholds
     params.minThreshold = 0;
-    params.maxThreshold = 256;
+    params.maxThreshold = 255;
 
     # Filter by Area.
     params.filterByArea = True
-    params.minArea = 30
+    params.minArea = 20
 
     # Filter by Circularity
     params.filterByCircularity = True
-    params.minCircularity = 0.01
+    params.minCircularity = 0.2
 
     # Filter by Convexity
     params.filterByConvexity = True
     params.minConvexity = 0.5
 
     # Filter by Inertia
-    params.filterByInertia =True
+    params.filterByInertia = False
     params.minInertiaRatio = 0.8
 
     detector = cv2.SimpleBlobDetector_create(params)
@@ -74,8 +93,8 @@ while cap.isOpened():
         # print("no blobs")
         score = 0
 
-    filt_score = (1-alpha)*prev_score + alpha*score
-    prev_score = filt_score
+    # filt_score = (1-alpha)*prev_score + alpha*score
+    # prev_score = filt_score
 
     # Draw green circles around detected blobs
     im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (255,0,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
@@ -83,14 +102,14 @@ while cap.isOpened():
     # Bitwise-AND mask and original image
     res = cv2.bitwise_and(im,im, mask= mask)
 
-    # cv2.imshow('masked Image',res)
-    if filt_score > 0.5:
-        # cv2.imshow('mask',mask)
-        cv2.imshow('Frame',im_with_keypoints)
-        print('Found tennis ball - prob =',filt_score)
-    else:
-        cv2.imshow('Frame',im)
-        print('No tennis ball - prob =',filt_score)
+    cv2.imshow('masked Image',res)
+    cv2.imshow('mask',mask)
+    cv2.imshow('Frame',im_with_keypoints)
+    # if filt_score > 0.5:
+        # print('Found tennis ball - prob =',filt_score)
+    # else:
+        # cv2.imshow('Frame',im)
+        # print('No tennis ball - prob =',filt_score)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
